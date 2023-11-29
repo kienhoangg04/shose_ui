58deca9edeba3388575630c6b35b802da77ee006
kienhoangg04 <kienkic0403@gmail.com>
2023-11-29 14:10:42 +0700

    update deploy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:3001',
+            target: 'https://kien-api-shose.onrender.com',
             changeOrigin: true,
         }),
     );
